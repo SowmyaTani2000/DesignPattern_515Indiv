7591600aa3807d7b07282397fa617c7169963a32
Sowmya <stanikon@asu.edu>
2022-10-07 23:40:16 -0700

    adding factorymethod dp

---
+++ factorymethod/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+
+	private ProductMenu theProductMenu;
+
+	private ProductMenu productMenu;
+
+	public abstract void showMenu();
+
+	public void showAddButton() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+	public void showLabels() {
+
+	}
+
+	public abstract ProductMenu CreateProductMenu();
+
+}
