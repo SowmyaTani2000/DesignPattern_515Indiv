a5fba1919c89a20f3be5f1f827876d1e94e30f17
Sowmya <stanikon@asu.edu>
2022-10-13 22:23:38 -0700

    adding classdiagram

---
+++ classdiagramptbs/ClassProductList.java
@@ -0,0 +1,17 @@
+public class ClassProductList {
+
+	private ReminderVisitor reminderVisitor;
+
+	private ReminderVisitor reminderVisitor;
+
+	private Product[] product;
+
+	private ProductIterator productIterator;
+
+	private ProductIterator productIterator;
+
+	public void accept(NodeVisitor visitor) {
+
+	}
+
+}
---
+++ classdiagramptbs/ReminderVisitor.java
@@ -0,0 +1,21 @@
+public class ReminderVisitor extends NodeVisitor {
+
+	private Reminder rm_Reminder;
+
+	private ClassProductList classProductList;
+
+	private ClassProductList classProductList;
+
+	public void visitProduct(Product product) {
+
+	}
+
+	public void visitTrading(Trading trading) {
+
+	}
+
+	public void visitFacade(Facade facade) {
+
+	}
+
+}
